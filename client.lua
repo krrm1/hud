9488fddfb20815390f548ad8fe9b6344142fbf4d
kareem_105 <hbk.dood@gmail.com>
2025-09-28 00:58:18 -0700

    Update client.lua
    vehicle checking list with black list for vehicle class

--- client.lua
+++ client.lua
@@ -10,16 +10,37 @@ local function HideHud()
     SendNUIMessage({event = "HideUI"})
 end
 
+function VehicleBlackList(vehicle)
+    local blackListClass = {
+        13 ,-- "Cycles"
+        15 ,-- "Helicopters"
+        16 ,-- "Planes"
+        21 ,-- "Trains"
+    }
+    local vehicleClass = GetVehicleClass(vehicle)
+
+    if vehicleClass then
+        for _, class in pairs(blackListClass) do
+            if vehicleClass == class then
+                return true
+            end
+        end
+    end
+end
+
 CreateThread(function()
     while true do
         local player = PlayerPedId()
         local vehicle = GetVehiclePedIsIn(player)
 
-        if IsPedInAnyVehicle(player) and GetIsVehicleEngineRunning(vehicle) then
+           
+        if IsPedInAnyVehicle(player) and GetIsVehicleEngineRunning(vehicle) and not VehicleBlackList(vehicle) then
             local vehicle = GetVehiclePedIsIn(player)
             local speed = math.ceil(GetEntitySpeed(vehicle) * speedMultiplier)
             local fuel = math.ceil(GetVehicleFuelLevel(vehicle))
             local engine = math.ceil(GetVehicleEngineHealth(vehicle)) / 10
+            
+        print(VehicleBlackList(vehicle))
 
             ShowHud()
             SendNUIMessage({
